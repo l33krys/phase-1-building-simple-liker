67b6675281a97b07de9584c7aa3c99499efa7b1a
Krystle Lee <l33.krys@gmail.com>
2023-08-07 12:39:19 -0700

    complete lab

--- main.js
+++ main.js
@@ -3,8 +3,25 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph')
 
-
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].addEventListener('click', (e) => {
+    mimicServerCall()
+    .then(() => {})
+    .catch(() => {
+      let errorMsg = document.querySelector('#modal')
+      errorMsg.classList.remove('hidden')
+      setTimeout(() => {errorMsg.classList.add('hidden')}, 3000)
+    })
+    if (e.target.classList.contains('activated-heart')) {
+      e.target.classList.remove('activated-heart')
+    } else {
+      e.target.classList.add('activated-heart')
+    }
+    
+  });
+}
 
 
 //------------------------------------------------------------------------------
